53dd09c3a68cbe5cc0411ce8901118b04be96298
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-06-15 11:19:56 -0300

    criando banco de dados

--- index.ts
+++ index.ts
@@ -3,9 +3,13 @@ import express from 'express';
 // eslint-disable-next-line import/no-unresolved
 import error from './src/middlewares/error.middleware';
 
+import BooksRoutes from './src/routes/books.routes';
+
 const app = express();
 
 app.use(express.json());
+
+app.use(BooksRoutes);
 
 app.use(error);
 
