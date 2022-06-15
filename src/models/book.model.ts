198f8e68889dc029016b2e15299e0bd0c2f33c23
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-06-15 11:53:30 -0300

    editando um livro

--- src/models/book.model.ts
+++ src/models/book.model.ts
@@ -36,4 +36,14 @@ export default class BookModel {
     const [book] = rows as Book[];
     return book;
   }
+
+  public async update(id: number, book: Book) {
+    const {
+      title, price, author, isbn,
+    } = book;
+    await this.connection.execute(
+      'UPDATE books SET title=?, price=?, author=?, isbn=? WHERE id=?',
+      [title, price, author, isbn, id],
+    );
+  }
 }
